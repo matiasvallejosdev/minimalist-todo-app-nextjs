ef7513995e380db852a2aa06f5fe5230e9cd6e94
Matias Vallejos <matiasvallejosdev@outlook.com>
2024-05-29 18:26:46 -0300

    Change lib folder to src/lib

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons"
 import { DayPicker } from "react-day-picker"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/src/lib/utils"
 import { buttonVariants } from "@/src/components/ui/button"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
